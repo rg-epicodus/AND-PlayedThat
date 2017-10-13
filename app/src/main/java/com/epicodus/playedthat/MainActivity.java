ddc05db5e45b9b02db4b548088cbd356abf03109
Rich Garrick <richg341@gmail.com>
2017-10-13 10:23:27 -0700

    add butterknife and typeface

--- app/src/main/java/com/epicodus/playedthat/MainActivity.java
+++ app/src/main/java/com/epicodus/playedthat/MainActivity.java
@@ -1,13 +1,25 @@
 package com.epicodus.playedthat;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface rubikFont = Typeface.createFromAsset(getAssets(), "fonts/rubikregular.ttf");
+        mAppNameTextView.setTypeface(rubikFont);
+
     }
 }
