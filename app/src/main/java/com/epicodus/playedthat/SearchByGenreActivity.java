43611ddc3576bcbd6a8c865b654ca9d1f5a677b0
Rich Garrick <richg341@gmail.com>
2017-10-13 12:44:48 -0700

    setup initial layout for gridview

--- app/src/main/java/com/epicodus/playedthat/SearchByGenreActivity.java
+++ app/src/main/java/com/epicodus/playedthat/SearchByGenreActivity.java
@@ -2,7 +2,11 @@ package com.epicodus.playedthat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,8 +17,19 @@ public class SearchByGenreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_games);
+        setContentView(R.layout.activity_searchbygenre);
         ButterKnife.bind(this);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(SearchByGenreActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
