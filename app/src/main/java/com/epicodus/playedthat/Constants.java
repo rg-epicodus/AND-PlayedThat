ce090bb20c0f7e4825b6cbc996e8e501fe2e32a6
Rich Garrick <richg341@gmail.com>
2017-10-19 12:32:31 -0700

    Update Constants.java

--- app/src/main/java/com/epicodus/playedthat/Constants.java
+++ app/src/main/java/com/epicodus/playedthat/Constants.java
@@ -6,7 +6,7 @@ package com.epicodus.playedthat;
 
 public class Constants {
     public static final String regCode= BuildConfig.regCode;
-    public static final String API_BASE_URL = "http://www.giantbomb.com/api/genres/?api_key=6faec9b0244c7409297a898e161cf3144668cdcd&format=json&field_list=name";
+    public static final String API_BASE_URL = "http://www.giantbomb.com/api/genres/?api_key=redacted&format=json&field_list=name";
 //    public static final String API_GENRES_QUERY_PARAMETER = "genres/";
 //    public static final String FIELD_LIST_QUERY_PARAMETER = "&field_list=";
 
